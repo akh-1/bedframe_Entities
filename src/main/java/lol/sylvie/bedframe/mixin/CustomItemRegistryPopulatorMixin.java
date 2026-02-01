acece6f78a1c676f444410b7fe930e7623a54b6e
Newlumberjack-sys <newlumberjack@nljack.xyz>
2026-01-31 21:07:24 -0600

    Support for newest Fabric Version + Updated Geyser-Fabric versions

--- src/main/java/lol/sylvie/bedframe/mixin/CustomItemRegistryPopulatorMixin.java
+++ src/main/java/lol/sylvie/bedframe/mixin/CustomItemRegistryPopulatorMixin.java
@@ -1,18 +1,13 @@
 package lol.sylvie.bedframe.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import org.cloudburstmc.nbt.NbtMap;
 import org.cloudburstmc.nbt.NbtMapBuilder;
-import org.cloudburstmc.nbt.NbtType;
-import org.geysermc.geyser.api.item.custom.CustomItemData;
 import org.geysermc.geyser.api.item.custom.NonVanillaCustomItemData;
-import org.geysermc.geyser.registry.BlockRegistries;
 import org.geysermc.geyser.registry.populator.CustomItemRegistryPopulator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /*
@@ -21,17 +16,45 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 @Mixin(CustomItemRegistryPopulator.class)
 public class CustomItemRegistryPopulatorMixin {
-    @Inject(method = "createComponentNbt(Lorg/geysermc/geyser/api/item/custom/NonVanillaCustomItemData;Ljava/lang/String;IZZI)Lorg/cloudburstmc/nbt/NbtMapBuilder;", at = @At(value = "INVOKE", target = "Lorg/geysermc/geyser/registry/populator/CustomItemRegistryPopulator;computeBlockItemProperties(Ljava/lang/String;Lorg/cloudburstmc/nbt/NbtMapBuilder;)V", shift = At.Shift.AFTER), remap = false)
-    private static void bedframe$removeBlockIconComponent(NonVanillaCustomItemData customItemData, String customItemName, int customItemId, boolean isHat, boolean displayHandheld, int protocolVersion, CallbackInfoReturnable<NbtMapBuilder> cir, @Local(ordinal = 1) NbtMapBuilder itemProperties) {
-        if (customItemData.icon().isEmpty()) itemProperties.remove("minecraft:icon");
+    // require = 0, prevents nasty crashes when Geyser makes changes :(
+    @Inject(method = "createComponentNbt(Lorg/geysermc/geyser/api/item/custom/NonVanillaCustomItemData;Ljava/lang/String;IZZI)Lorg/cloudburstmc/nbt/NbtMapBuilder;",
+            at = @At(value = "INVOKE", target = "Lorg/geysermc/geyser/registry/populator/CustomItemRegistryPopulator;computeBlockItemProperties(Ljava/lang/String;Lorg/cloudburstmc/nbt/NbtMapBuilder;)V", shift = At.Shift.AFTER),
+            remap = false, require = 0)
+    private static void bedframe$applyBlockFix(NonVanillaCustomItemData customItemData, String customItemName, int customItemId, boolean isHat, boolean displayHandheld, int protocolVersion, CallbackInfoReturnable<NbtMapBuilder> cir, @Local(ordinal = 1) NbtMapBuilder itemProperties) {
+        bedframe$applyBlockIconFix(customItemData, itemProperties);
     }
 
-    @Inject(method = "computeBlockItemProperties", at=@At("HEAD"), cancellable = true, remap = false)
-    private static void bedframe$addBlockPlacerComponent(String blockItem, NbtMapBuilder componentBuilder, CallbackInfo ci) {
-        componentBuilder.putCompound("minecraft:block_placer", NbtMap.builder()
-                .putString("block", blockItem)
-                .putBoolean("canUseBlockAsIcon", true)
-                .build());
-        ci.cancel();
+    @Inject(method = "createComponentNbt(Lorg/geysermc/geyser/api/item/custom/NonVanillaCustomItemData;Ljava/lang/String;IZZI)Lorg/cloudburstmc/nbt/NbtMapBuilder;",
+            at = @At(value = "INVOKE", target = "Lorg/geysermc/geyser/registry/populator/CustomItemRegistryPopulator;computeBlockItemProperties(Lorg/geysermc/geyser/api/item/custom/v2/component/geyser/BlockPlacer;Lorg/cloudburstmc/nbt/NbtMapBuilder;)V", shift = At.Shift.AFTER),
+            remap = false, require = 0)
+    private static void bedframe$applyBlockFixV2(NonVanillaCustomItemData customItemData, String customItemName, int customItemId, boolean isHat, boolean displayHandheld, int protocolVersion, CallbackInfoReturnable<NbtMapBuilder> cir, @Local(ordinal = 1) NbtMapBuilder itemProperties) {
+        bedframe$applyBlockIconFix(customItemData, itemProperties);
+    }
+
+    @Inject(method = "createComponentNbt(Lorg/geysermc/geyser/api/item/custom/NonVanillaCustomItemData;Ljava/lang/String;IZZ)Lorg/cloudburstmc/nbt/NbtMapBuilder;",
+            at = @At(value = "INVOKE", target = "Lorg/geysermc/geyser/registry/populator/CustomItemRegistryPopulator;computeBlockItemProperties(Ljava/lang/String;Lorg/cloudburstmc/nbt/NbtMapBuilder;)V", shift = At.Shift.AFTER),
+            remap = false, require = 0)
+    private static void bedframe$applyBlockFixLegacy(NonVanillaCustomItemData customItemData, String customItemName, int customItemId, boolean isHat, boolean displayHandheld, CallbackInfoReturnable<NbtMapBuilder> cir, @Local(ordinal = 1) NbtMapBuilder itemProperties) {
+        bedframe$applyBlockIconFix(customItemData, itemProperties);
+    }
+
+    @Inject(method = "createComponentNbt(Lorg/geysermc/geyser/api/item/custom/NonVanillaCustomItemData;Ljava/lang/String;IZZ)Lorg/cloudburstmc/nbt/NbtMapBuilder;",
+            at = @At(value = "INVOKE", target = "Lorg/geysermc/geyser/registry/populator/CustomItemRegistryPopulator;computeBlockItemProperties(Lorg/geysermc/geyser/api/item/custom/v2/component/geyser/BlockPlacer;Lorg/cloudburstmc/nbt/NbtMapBuilder;)V", shift = At.Shift.AFTER),
+            remap = false, require = 0)
+    private static void bedframe$applyBlockFixLegacyV2(NonVanillaCustomItemData customItemData, String customItemName, int customItemId, boolean isHat, boolean displayHandheld, CallbackInfoReturnable<NbtMapBuilder> cir, @Local(ordinal = 1) NbtMapBuilder itemProperties) {
+        bedframe$applyBlockIconFix(customItemData, itemProperties);
+    }
+
+    private static void bedframe$applyBlockIconFix(NonVanillaCustomItemData customItemData, NbtMapBuilder itemProperties) {
+        String blockItem = customItemData.block();
+        if (blockItem != null && !blockItem.isBlank()) {
+            if (customItemData.icon().isEmpty()) {
+                itemProperties.remove("minecraft:icon");
+            }
+            itemProperties.putCompound("minecraft:block_placer", NbtMap.builder()
+                    .putString("block", blockItem)
+                    .putBoolean("canUseBlockAsIcon", true)
+                    .build());
+        }
     }
 }
